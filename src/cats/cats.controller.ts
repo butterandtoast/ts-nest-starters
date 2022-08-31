664ac142e0d30ab9ada0d64b03e25e5065095b0f
F7T07SV <akshat.jain@fiserv.com>
2022-08-31 14:20:49 +0100

    adding the cats [nest g resource cats]

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,35 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+
+@Controller()
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @MessagePattern('createCat')
+  create(@Payload() createCatDto: CreateCatDto) {
+    return this.catsService.create(createCatDto);
+  }
+
+  @MessagePattern('findAllCats')
+  findAll() {
+    return this.catsService.findAll();
+  }
+
+  @MessagePattern('findOneCat')
+  findOne(@Payload() id: number) {
+    return this.catsService.findOne(id);
+  }
+
+  @MessagePattern('updateCat')
+  update(@Payload() updateCatDto: UpdateCatDto) {
+    return this.catsService.update(updateCatDto.id, updateCatDto);
+  }
+
+  @MessagePattern('removeCat')
+  remove(@Payload() id: number) {
+    return this.catsService.remove(id);
+  }
+}
